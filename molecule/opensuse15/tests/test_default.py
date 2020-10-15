d5cabbc8eac4a20db0bf69e7447f2544fae74272
Erik Neubauer <49983818+enbrnz@users.noreply.github.com>
2020-10-15 18:11:34 +0200

    added suse configuration and testing; drone configuration still missing

---
+++ molecule/opensuse15/tests/test_default.py
@@ -0,0 +1,24 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_mariadb_running_and_enabled(host):
+    mariadb = host.service("mariadb")
+    assert mariadb.is_running
+    assert mariadb.is_enabled
+
+
+def test_mariadb_config(host):
+    config = host.run("/usr/bin/mysqladmin variables | tr -d ' '").stdout
+
+    assert "|datadir|/var/lib/mysql-files/|" in config
+    assert "|port|3306|" in config
+    assert "|socket|/run/mysql/mysql.sock|" in config
+    assert "|pid_file|/var/lib/mysql/mysqld.pid|" in config
+    assert "|tx_isolation|READ-COMMITTED|" in config
+    assert "|log_error|/var/log/mysql/mysqld.log|" in config
