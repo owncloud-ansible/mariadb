3c891ad65cfd8c6ce4a7f070f124ce7da3ae8d00
Robert Kaussow <mail@thegeeklab.de>
2022-02-16 11:37:25 +0100

    feat: add option mariadb_environment_isolation for RH 7 based systems (#19)
    BREAKING CHANGE: For RedHat 7 based distributions, MariaDB packages `rh-mariadb101*` were updated to `rh-mariadb103*`. Depending on your data directory configuration, you may need to migrate it manually after the upgrade.

--- molecule/centos7/tests/test_default.py
+++ molecule/centos7/tests/test_default.py
@@ -8,17 +8,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_mariadb_running_and_enabled(host):
-    mariadb = host.service("rh-mariadb101-mariadb")
+    mariadb = host.service("rh-mariadb103-mariadb")
     assert mariadb.is_running
     assert mariadb.is_enabled
 
 
-def test_mariadb_config(host):
-    config = host.run("/opt/rh/rh-mariadb101/root/bin/mysqladmin variables | tr -d ' '").stdout
+def test_mariadb_syspaths(host):
+    mysql_client = host.file("/usr/bin/mysql")
+    mysql_version = host.run("/usr/bin/mysql --version").stdout
 
-    assert "|datadir|/var/opt/rh/rh-mariadb101/lib/mysql/|" in config
+    assert mysql_client.exists
+    assert "/opt/rh/rh-mariadb103/root/usr/bin/mysql" in mysql_version
+    assert "Distrib 10.3" in mysql_version
+
+
+def test_mariadb_config(host):
+    config = host.run("/opt/rh/rh-mariadb103/root/bin/mysqladmin variables | tr -d ' '").stdout
+
+    assert "|datadir|/var/opt/rh/rh-mariadb103/lib/mysql/|" in config
     assert "|port|3306|" in config
     assert "|socket|/var/lib/mysql/mysql.sock|" in config
-    assert "|pid_file|/var/run/rh-mariadb101-mariadb/mariadb.pid|" in config
+    assert "|pid_file|/var/run/rh-mariadb103-mariadb/mariadb.pid|" in config
     assert "|tx_isolation|READ-COMMITTED|" in config
-    assert "|log_error|/var/opt/rh/rh-mariadb101/log/mariadb/mariadb.log|" in config
+    assert "|log_error|/var/opt/rh/rh-mariadb103/log/mariadb/mariadb.log|" in config
